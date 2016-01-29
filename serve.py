720c386823648fb7ae9d476cce528e10904e1187
Deepak Dubey <deepakdubey597@gmail.com>
2016-01-29 12:49:14 +0530

    minor changes

--- serve.py
+++ serve.py
@@ -1,11 +1,10 @@
 import os
 import sys
 import waitress
+from app import app
 
 BASE_DIR = os.path.join(os.path.dirname(__file__), 'src')
 sys.path.append(BASE_DIR)
-
-from app import app
 waitress.serve(
     app,
     host='0.0.0.0',
