e42d414094ec070d8afbcb75c080f57effe4aa34
Deepak Dubey <deepakdubey597@gmail.com>
2016-01-29 11:52:33 +0530

    updated

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 import config
 from app import app, db
+from waitress import serve
 
 
 os.environ['APP_SETTINGS']="config.DevelopmentConfig"
@@ -11,6 +12,14 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
+"""
 if __name__ == '__main__':
     manager.run()
+"""
+if __name__ == "__main__":
+    os.environ['APP_SETTINGS']="config.DevelopmentConfig"
+    manager.run()
+    #app.run(host='0.0.0.0', port=port)
+else:
+    os.environ['APP_SETTINGS']="config.ProductionConfig"
+    serve(app, port=port)
