c2df338da367c74b8b1102b477662434bd646d3b
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-03 15:57:12 +0530

    rename app file

--- production.py
+++ production.py
@@ -2,13 +2,13 @@ import os
 from config import Config
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-os.environ['DATABASE_URL'] = 'postgresql+psycopg2://peg:1234@localhost/app'
+os.environ['DATABASE_URL'] = 'postgres://username:pass1234/app'
 
 class ProductionConfig(Config):
     DEBUG = False
     SECRET_KEY = 'my_precious'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peg:1234@localhost/app'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     DEBUG_TB_ENABLED = False
     STRIPE_SECRET_KEY = 'foo'
     STRIPE_PUBLISHABLE_KEY = 'bar'
