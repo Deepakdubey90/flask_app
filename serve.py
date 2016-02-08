8060123523a198bf82d675ef950a16f9124f751a
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-08 18:52:01 +0530

    updated  production

--- serve.py
+++ serve.py
@@ -8,6 +8,4 @@ sys.path.append(BASE_DIR)
 waitress.serve(
     app,
     host='0.0.0.0',
-    port=os.getenv('PORT'),
-    cleanup_interval=os.getenv('CLEANUP_INTERVAL', 20),
-    channel_timeout=os.getenv('CHANNEL_TIMEOUT', 20))
+    port=os.getenv('PORT'))
