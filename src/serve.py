4ecbbe2edbbe76c32c6324f47546ee0fda1c5caa
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-11 17:10:24 +0530

    monor changes

--- src/serve.py
+++ src/serve.py
@@ -3,8 +3,10 @@ import sys
 import waitress
 from app import app
 
+
 BASE_DIR = os.path.join(os.path.dirname(__file__), 'src')
 sys.path.append(BASE_DIR)
+
 waitress.serve(
     app,
     host='0.0.0.0',
