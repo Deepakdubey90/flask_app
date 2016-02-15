b32c9c0d0e1d0c2590a3c05ea277c262f0309938
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-15 15:56:17 +0530

    updated serializer for blog.

---
+++ src/feature/serializers.py
@@ -0,0 +1,28 @@
+from marshmallow import Schema, fields, pprint
+from models import User
+
+
+class UserSchema(Schema):
+    """
+    user schema for serialization & deserialization.
+    """
+    id = fields.Integer()
+    username = fields.Str()
+    first_name = fields.Str()
+    last_name = fields.Str()
+    email = fields.Str()
+    password = fields.Str()
+
+    class Meta:
+        fields = ("id", "username", "first_name", "last_name", "email")
+        #exclude = ('password',)
+
+
+user_schema = UserSchema()
+
+def user_deserializer(data):
+    return user_schema.load(data).data
+
+def user_after_get_many(result=None, search_params=None, **kw):
+    #import pdb;pdb.set_trace();
+    result['objects'] = [user_deserializer(obj) for obj in result['objects']]
