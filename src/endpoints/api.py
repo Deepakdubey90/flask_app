a3c6d82afd1bde8837e9af6c2519fc521d6dfce8
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-15 14:23:08 +0530

    Implemented custom serializer for user module.

--- src/endpoints/api.py
+++ src/endpoints/api.py
@@ -1,6 +1,7 @@
 from app import app, db
 from flask.ext.restless import APIManager
 from user.models import User
+from user.serializers import user_after_get_many
 from feature.models import Feature
 from document.models import Document
 from blog.models import Blog
@@ -10,9 +11,12 @@ from blog.models import Blog
 methods = ['GET', 'POST', 'PUT', 'DELETE']
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
-user_api = manager.create_api_blueprint(User, app=app, methods=methods, exclude_columns='password',
+user_api = manager.create_api_blueprint(User, app=app, methods=methods,
                                         url_prefix='/api/v1', results_per_page=10,
-                                        max_results_per_page=100)
+                                        max_results_per_page=100,
+                                        postprocessors={
+                                            'GET_MANY':[user_after_get_many]
+                                        })
 
 blog_api = manager.create_api_blueprint(Blog, app=app, methods=methods,
                                         url_prefix='/api/v1', results_per_page=10,
