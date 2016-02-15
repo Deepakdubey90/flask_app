66e7f457b66265bb656fd7289ea899a7fdc3ea8a
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-15 12:44:24 +0530

    created endpoints & register with url.

---
+++ src/endpoints.py
@@ -0,0 +1,27 @@
+from app import app, db
+from flask.ext.restless import APIManager
+from user.models import User
+from feature.models import Feature
+from document.models import Document
+from blog.models import Blog
+
+
+# API Creation.
+methods = ['GET', 'POST', 'PUT', 'DELETE']
+
+manager = APIManager(app, flask_sqlalchemy_db=db)
+user_api = manager.create_api_blueprint(User, app=app, methods=methods,
+                                        url_prefix='/api', results_per_page=10,
+                                        max_results_per_page=100)
+
+blog_api = manager.create_api_blueprint(Blog, app=app, methods=methods,
+                                        url_prefix='/api', results_per_page=10,
+                                        max_results_per_page=100)
+
+feature_api = manager.create_api_blueprint(Feature, app=app, methods=methods,
+                                           url_prefix='/api', results_per_page=10,
+                                           max_results_per_page=100)
+
+document_api = manager.create_api_blueprint(Document, app=app, methods=methods,
+                                            url_prefix='/api', results_per_page=10,
+                                            max_results_per_page=100)
