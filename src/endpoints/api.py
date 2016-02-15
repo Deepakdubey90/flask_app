a39daaa043aefd88096891f0a84d1db36a06a5de
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-15 12:57:30 +0530

    Added endpoints

--- src/endpoints/api.py
+++ src/endpoints/api.py
@@ -10,18 +10,18 @@ from blog.models import Blog
 methods = ['GET', 'POST', 'PUT', 'DELETE']
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
-user_api = manager.create_api_blueprint(User, app=app, methods=methods,
-                                        url_prefix='/api', results_per_page=10,
+user_api = manager.create_api_blueprint(User, app=app, methods=methods, exclude_columns='password',
+                                        url_prefix='/api/v1', results_per_page=10,
                                         max_results_per_page=100)
 
 blog_api = manager.create_api_blueprint(Blog, app=app, methods=methods,
-                                        url_prefix='/api', results_per_page=10,
+                                        url_prefix='/api/v1', results_per_page=10,
                                         max_results_per_page=100)
 
 feature_api = manager.create_api_blueprint(Feature, app=app, methods=methods,
-                                           url_prefix='/api', results_per_page=10,
+                                           url_prefix='/api/v1', results_per_page=10,
                                            max_results_per_page=100)
 
 document_api = manager.create_api_blueprint(Document, app=app, methods=methods,
-                                            url_prefix='/api', results_per_page=10,
+                                            url_prefix='/api/v1', results_per_page=10,
                                             max_results_per_page=100)
--- src/urls.py
+++ src/urls.py
@@ -4,14 +4,13 @@ This module bind views with url.
 from app import app, db
 from flask.ext.restless import APIManager
 from user.views import user_module
-from endpoints import (
+from endpoints.api import (
     user_api, blog_api,
     feature_api, document_api
 )
 
 # url registration.
 app.register_blueprint(user_module) # registering Blueprint
-
 
 # registering API
 app.register_blueprint(user_api)
