6fb69010be41466707b3d6cea185df8f8a2e141b
Deepak Dubey <deepakdubey597@gmail.com>
2016-02-15 15:55:48 +0530

    updated serializer for blog.

---
+++ src/blog/serializers.py
@@ -0,0 +1,24 @@
+from marshmallow import Schema, fields, pprint
+from models import Blog
+
+
+class BlogSchema(Schema):
+    """
+    user schema for serialization & deserialization.
+    """
+    id = fields.Integer()
+    account_number = fields.Str()
+    url = fields.Str()
+
+    class Meta:
+        fields = ("id", "account_number", "url", "create_on", "updated_on", "feature")
+        #exclude = ('password',)
+
+
+blog_schema = BlogSchema()
+
+def blog_deserializer(data):
+    return blog_schema.load(data).data
+
+def blog_after_get_many(result=None, search_params=None, **kw):
+    result['objects'] = [blog_deserializer(obj) for obj in result['objects']]
